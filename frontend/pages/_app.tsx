f581de8ff064757cbc2e827243fdd159354c616a
toddbomb <tusharagg12@gmail.com>
2023-06-26 03:48:13 -0400

    include mobile view

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -5,6 +5,8 @@ import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
 import '@/styles/globals.css';
 import { ClerkProvider, SignedIn, SignedOut, SignIn, UserButton } from '@clerk/nextjs';
+import { BrowserView, MobileView, isBrowser, isMobile } from 'react-device-detect';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,29 +19,40 @@ function App({ Component, pageProps }: AppProps<{}>) {
       style={{ background: `url('/bkd.png') no-repeat center center fixed` }}
     >
       <Toaster />
-      <ClerkProvider
-        appearance={
-          {
-          layout: {
-            logoImageUrl: "logo2.png",
-            logoPlacement: "inside",
-              }
+      <BrowserView>
+        <ClerkProvider
+          appearance={
+            {
+            layout: {
+              logoImageUrl: "logo2.png",
+              logoPlacement: "inside",
+                }
+            }
           }
-        }
-      >
-        <div className="flex flex-col justify-center items-center min-h-screen">
-          <div className="flex items-center mb-4">
-            <SignedIn>
-              <QueryClientProvider client={queryClient}>
-                <Component {...pageProps} />
-              </QueryClientProvider>
-            </SignedIn>
-            <SignedOut>
-              <SignIn />
-            </SignedOut>
+        >
+          <div className="flex flex-col justify-center items-center min-h-screen">
+            <div className="flex items-center mb-4">
+              <SignedIn>
+                <QueryClientProvider client={queryClient}>
+                  <Component {...pageProps} />
+                </QueryClientProvider>
+              </SignedIn>
+              <SignedOut>
+                <SignIn />
+              </SignedOut>
+            </div>
+          </div>
+        </ClerkProvider>
+      </BrowserView>
+      <MobileView>
+        <div className="flex items-center justify-center h-screen">
+          <div className="text-center">
+            <h1 style={{ fontFamily: 'Roboto Mono, sans-serif' }} className="text-1xl font-bold mb-4">Thanks for visiting our site!</h1>
+            <p style={{ fontFamily: 'Roboto Mono, sans-serif' }} className="text-sm mb-1">We’re still configuring for mobile...</p>
+            <p style={{ fontFamily: 'Roboto Mono, sans-serif' }} className="text-sm mb-4">Until then, please visit us on desktop. Thanks!</p>
           </div>
         </div>
-      </ClerkProvider>
+      </MobileView>
     </div>
   );
 }
