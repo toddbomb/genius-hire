a1e4495cc782a8314017bc3115666fa5d4529168
toddbomb <tusharagg12@gmail.com>
2023-06-25 23:25:09 -0400

    Update query_data.py

--- backend/src/util/query_data.py
+++ backend/src/util/query_data.py
@@ -6,29 +6,16 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import PromptTemplate
 from langchain.vectorstores import Pinecone
 from langchain.chains import RetrievalQA
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
 
+os.environ['OPENAI_API_KEY'] = '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'
 
 
 def get_pdf_qa_chain_response(query, job_description, filename):
 
     chatgpt_model = ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo-16k')
 
-
-    prompt_template = f"""Use the following pieces of context to answer the question at the end. If you don't know the 
-    answer, just say that you don't know, don't try to make up an answer.
-
-    Job Description:
-    {job_description}
-
-    ```{{context}}```
-
-    Question: {{question}}
-    Helpful Answer: """
-
-
-    qa_prompt = PromptTemplate(
-        template=prompt_template, input_variables=["context", "question"]
-    )
     embeddings = OpenAIEmbeddings()
 
     pinecone.init(
@@ -39,13 +26,35 @@ def get_pdf_qa_chain_response(query, job_description, filename):
 
     vectorstore = Pinecone.from_existing_index(index_name, embeddings, namespace=filename)
 
+
+    prompt_template = f"""You are an experienced hiring manager for a company. You will be given a job description and the resumes of candidates 
+    to answer the user's question. Answer questions objectively, using the content from the job description to back up your answers.
+      If the topic requested isn't in a candidate's resume, you can assume the candidate has no experience with the topic. 
+      If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+    Job Description:
+    {job_description}
+
+    Resumes:
+    ```{{context}}```
+
+    Question: {{question}}
+    Helpful Answer: """
+
+
+    qa_prompt = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
     chain_type_kwargs = {"prompt": qa_prompt}
 
-    pdf_qa = RetrievalQA.from_chain_type(
+    pdf_qa = ConversationalRetrievalChain.from_llm(
         llm=chatgpt_model,
-        chain_type="stuff",
-        retriever=vectorstore.as_retriever(),
-        chain_type_kwargs=chain_type_kwargs
+        retriever=vectorstore.as_retriever(search_kwargs={'k': 50}),
+        memory=memory,
+        combine_docs_chain_kwargs={"prompt": qa_prompt}
     )
 
     result = pdf_qa.run(query)
@@ -79,4 +88,8 @@ Track record of delivering successful products / working in a collaborative, fas
 Join the AI revolution with Instalily! As an AI Engineer, you will have the opportunity to make a significant impact on the future of AI and work with a talented and passionate team of experts. If you’re ready to take your software engineering skills to the next level, we want to hear from you!
 """
 
-filename = "e"
+filename = "exresumes.pdf"
+
+query = "Rank order the top 5 candidates that are best fit for this job and assign a numerical grade out of 100 to each. "
+
+print(get_pdf_qa_chain_response(query, job_description, filename))
