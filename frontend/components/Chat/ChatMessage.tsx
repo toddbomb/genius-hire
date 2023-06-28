72a5fb5f6ecf41b4dcb029fedde2dab926fa7588
CristinConnerney <cristin@instalily.ai>
2023-06-28 17:22:35 -0400

    update

--- frontend/components/Chat/ChatMessage.tsx
+++ frontend/components/Chat/ChatMessage.tsx
@@ -216,30 +216,6 @@ export const ChatMessage: FC<Props> = memo(({ message, messageIndex, onEdit }) =
                 remarkPlugins={[remarkGfm, remarkMath]}
                 rehypePlugins={[rehypeMathjax]}
                 components={{
-                  code({ node, inline, className, children, ...props }) {
-                    if (children.length) {
-                      if (children[0] == '▍') {
-                        return <span className="animate-pulse cursor-default mt-1">▍</span>
-                      }
-
-                      children[0] = (children[0] as string).replace("`▍`", "▍")
-                    }
-
-                    const match = /language-(\w+)/.exec(className || '');
-
-                    return !inline ? (
-                      <CodeBlock
-                        key={Math.random()}
-                        language={(match && match[1]) || ''}
-                        value={String(children).replace(/\n$/, '')}
-                        {...props}
-                      />
-                    ) : (
-                      <code className={className} {...props}>
-                        {children}
-                      </code>
-                    );
-                  },
                   table({ children }) {
                     return (
                       <table className="border-collapse border border-black px-3 py-1 dark:border-white">
